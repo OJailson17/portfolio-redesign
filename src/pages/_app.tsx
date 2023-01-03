7f6687e0ab736269daf08dcb240ddfd5da05915f
OJailson17 <JaylsonO17@gmail.com>
2023-01-03 01:05:35 -0300

    chore: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { ThemeContextProvider } from '../context/themeContext';
+import * as gtag from '../utils/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url: URL) => {
+			gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<ThemeContextProvider>
 			<Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../utils/gtag';
 
 export default function Document() {
 	return (
 		<Html lang='pt-BR'>
 			<Head>
+				{/* Global Site Tag (gtag.js) - Google Analytics */}
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+					}}
+				/>
+
+				{/* Google fonts */}
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
 					rel='preconnect'
