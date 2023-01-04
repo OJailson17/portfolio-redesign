47baf05721534de00425b5d881330b8bbfbdacab
OJailson17 <JaylsonO17@gmail.com>
2023-01-03 23:17:55 -0300

    chore: improve ceo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,13 +28,29 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
 				<meta
+					property='og:site_name'
+					content='Jailson de Oliveira | Portfólio'
+				></meta>
+				<meta
 					name='description'
 					content='Portfólio pessoal contendo as principais habilidades e experiências de Jailson de Oliveira em desenvolvimento web, além também de exemplos de projetos pessoais.'
 				/>
+				<meta
+					property='og:description'
+					content='Portfólio pessoal contendo as principais habilidades e experiências de Jailson de Oliveira em desenvolvimento web, além também de exemplos de projetos pessoais.'
+				></meta>
+				<meta
+					property='og:image'
+					content='https://github.com/OJailson17.png'
+				></meta>
 				<meta name='author' content='Jailson de Oliveira'></meta>
 				<meta
 					name='keywords'
 					content='Desenvolvedor Web, Desenvolvedor frontend, Frontend, Javascript, React, NextJs, Typescript, Programador, Utinga, Bahia'
+				></meta>
+				<meta
+					property='og:url'
+					content='https://jailsondeoliveira.vercel.app'
 				></meta>
 
 				<meta name='msapplication-TileColor' content='#ffffff' />
