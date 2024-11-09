4eb4265e6de052909792b5389a7978ca3e11dbcc
OJailson17 <JaylsonO17@gmail.com>
2024-11-08 23:21:00 -0300

    chore: add metatag for google verification

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
 		<>
 			<meta charSet='UTF-8' />
 			<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+			<meta
+				name='google-site-verification'
+				content='mNHo0kVRWw-LyGwyJpcuIaIV3iEzKPtwrLB0IqxglCA'
+			/>
 		</>
 	);
 }
